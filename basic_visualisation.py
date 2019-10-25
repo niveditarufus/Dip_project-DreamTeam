68e7b5615a742b6d0042c1934cca50c0af45ddba
svshivapuja <40575164+svshivapuja@users.noreply.github.com>
2019-10-25 22:21:07 +0530

    Update basic_visualisation.py

--- basic_visualisation.py
+++ basic_visualisation.py
@@ -1,3 +1,14 @@
+import cv2
+from PIL import Image
+import numpy as np
+from matplotlib import pyplot as plt
+#importing images
+img1 = cv2.imread('./input images/figure1.png',0)
+cx,cy = img1.shape
+#generate a hanning window
+hanw = cv2.createHanningWindow((cx,cy),cv2.CV_64F)
+
+
 
 #read the image and find polar map using the logPolar function to the magnitude spectrum
 
