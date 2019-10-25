13669f259a029db4237587e59e6c73364c2032f8
svshivapuja <40575164+svshivapuja@users.noreply.github.com>
2019-10-25 22:20:32 +0530

    Update basic_visualisation.py

--- basic_visualisation.py
+++ basic_visualisation.py
@@ -12,19 +12,6 @@ hanw = cv2.createHanningWindow((cx,cy),cv2.CV_64F)
 
 #read the image and find polar map using the logPolar function to the magnitude spectrum
 
-img1 = img1 * hanw
-#Do fft
-f1 = np.fft.fft2(img1)
-fshift1 = np.fft.fftshift(f1)
-magnitude_spectrum1 = np.abs(fshift1)
-log_magnitude_spectrum1 = 10*np.log(np.abs(fshift1) +1)
-magnitude_spectrum1 = 15*np.log(np.abs(fshift1) +1)
-polar_map1= cv2.linearPolar(magnitude_spectrum1, (cx/2,cy/2), cx/np.log(cx), flags=cv2.INTER_LINEAR+cv2.WARP_FILL_OUTLIERS)
-log_polar_map1= cv2.linearPolar(log_magnitude_spectrum1, (cx/2,cy/2), cx/np.log(cx), flags=cv2.INTER_LINEAR+cv2.WARP_FILL_OUTLIERS)
-cv2.imwrite('./output/log_mag1.png',log_magnitude_spectrum1)
-cv2.imwrite('./output/mag1.png',magnitude_spectrum1)
-cv2.imwrite('./output/log_polar1.png',log_polar_map1)
-cv2.imwrite('./output/polar_map1.png',polar_map1)
 
 
 
